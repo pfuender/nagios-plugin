7cc91fb50ebff21a75bd1e10c2aac5bf537d9a03
Frank Brehm <frank.brehm@profitbricks.com>
2012-11-26 17:02:03 +0100

    Added states to nagios/__init__.py

--- nagios/__init__.py
+++ nagios/__init__.py
@@ -4,14 +4,72 @@
 @author: Frank Brehm
 @contact: frank.brehm@profitbricks.com
 @copyright: (c) 2010 - 2012 by Frank Brehm, Berlin
-@summary: All modules for wrapper classes for different block devices
+@summary: Base modules for Nagios check plugins written in Python
 """
 
 __author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
 __copyright__ = '(C) 2010-2012 by profitbricks.com'
 __contact__ = 'frank.brehm@profitbricks.com'
-__version__ = '0.2.1-1'
+__version__ = '0.1.0-1'
 __license__ = 'GPL3'
+
+@property
+def bla():
+    """Test."""
+    return 99
+
+#==============================================================================
+
+class BaseNagiosError(Exception):
+    pass
+
+#==============================================================================
+
+def constant(f):
+
+    #------------------------------------------------------------
+    def fset(self, value):
+        raise SyntaxError('Constants may not changed.')
+
+    #------------------------------------------------------------
+    def fget(self):
+        return f()
+
+    #------------------------------------------------------------
+    return property(fget, fset)
+
+#==============================================================================
+
+class _State(object):
+
+    #------------------------------------------------------------
+    @constant
+    def ok():
+        return 0
+
+    #------------------------------------------------------------
+    @constant
+    def warning():
+        return 1
+
+    #------------------------------------------------------------
+    @constant
+    def critical():
+        return 2
+
+    #------------------------------------------------------------
+    @constant
+    def unknown():
+        return 3
+
+    #------------------------------------------------------------
+    @constant
+    def dependend():
+        return 4
+
+#==============================================================================
+
+state = _State()
 
 #==============================================================================
 
