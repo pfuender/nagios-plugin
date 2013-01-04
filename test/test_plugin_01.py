db71de2296051653246e8f4b0030ba20c06073fd
Frank Brehm <frank.brehm@profitbricks.com>
2013-01-04 14:49:04 +0100

    More tests on NagiosPlugin objects.

--- test/test_plugin_01.py
+++ test/test_plugin_01.py
@@ -14,6 +14,8 @@ import os
 import sys
 import logging
 
+from collections import OrderedDict
+
 libdir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 sys.path.insert(0, libdir)
 
@@ -26,6 +28,8 @@ from nagios import FakeExitError
 
 from nagios.plugin import NagiosPluginError
 from nagios.plugin import NagiosPlugin
+
+from nagios.plugin.threshold import NagiosThreshold
 
 from nagios.plugin.config import NoConfigfileFound
 
@@ -52,74 +56,79 @@ class TestNagiosPlugin(NeedConfig):
         log.debug("NagiosPluginArgparse object: %r", plugin)
         log.debug("NagiosPluginArgparse object: %s", str(plugin))
 
-#    #--------------------------------------------------------------------------
-#    def test_argparse_show_version(self):
-#
-#        log.info("Testing NagiosPluginArgparse showing version.")
-#        na = NagiosPluginArgparse(
-#                usage = '%(prog)s --version',
-#                url = 'http://www.profitbricks.com',
-#                blurb = 'Senseless sample Nagios plugin.',
-#                extra = 'Bla blub',
-#        )
-#
-#        try:
-#            na.parse_args(['--version'])
-#        except FakeExitError, e:
-#            log.debug("NagiosPluginArgparse exited with exit value %d.", e.exit_value)
-#            log.debug("Message on exit: >>>%s<<<", e.msg)
-#
-#    #--------------------------------------------------------------------------
-#    def test_argparse_show_usage(self):
-#
-#        log.info("Testing NagiosPluginArgparse showing usage.")
-#        na = NagiosPluginArgparse(
-#                usage = '%(prog)s --version',
-#                url = 'http://www.profitbricks.com',
-#                blurb = 'Senseless sample Nagios plugin.',
-#                extra = 'Bla blub',
-#        )
-#
-#        try:
-#            na.parse_args(['--usage'])
-#        except FakeExitError, e:
-#            log.debug("NagiosPluginArgparse exited with exit value %d.", e.exit_value)
-#            log.debug("Message on exit: >>>%s<<<", e.msg)
-#
-#    #--------------------------------------------------------------------------
-#    def test_argparse_parse_help(self):
-#
-#        log.info("Testing NagiosPluginArgparse generating help.")
-#        na = NagiosPluginArgparse(
-#                usage = '%(prog)s --help',
-#                url = 'http://www.profitbricks.com',
-#                blurb = 'Senseless sample Nagios plugin.',
-#                extra = 'Bla blub\n\nblubber blub',
-#                licence = 'Licence: GNU Lesser General Public License (LGPL), Version 3',
-#        )
-#
-#        try:
-#            na.parse_args(['-h'])
-#        except FakeExitError, e:
-#            log.debug("NagiosPluginArgparse exited with exit value %d.", e.exit_value)
-#            log.debug("Message on exit: >>>%s<<<", e.msg)
-#
-#    #--------------------------------------------------------------------------
-#    def test_argparse_wrong_argument(self):
-#
-#        log.info("Testing NagiosPluginArgparse for a wrong argument.")
-#        na = NagiosPluginArgparse(
-#                usage = '%(prog)s',
-#                url = 'http://www.profitbricks.com',
-#                blurb = 'Senseless sample Nagios plugin.',
-#                licence = '',
-#        )
-#
-#        try:
-#            na.parse_args(['--bli-bla-blub'])
-#        except FakeExitError, e:
-#            log.debug("NagiosPluginArgparse exited with exit value %d.", e.exit_value)
-#            log.debug("Message on exit: >>>%s<<<", e.msg)
+    #--------------------------------------------------------------------------
+    def test_plugin_object_props(self):
+
+        log.info("Testing NagiosPlugin object properties.")
+
+        plugin = NagiosPlugin()
+
+        if not isinstance(plugin, NagiosPlugin):
+            self.fail("Not a NagiosPlugin object: %r" % (plugin))
+
+        log.debug("Setting shortname explicitly to 'PAGESIZE'.")
+        plugin.shortname = "PAGESIZE"
+        self.assertEqual(plugin.shortname, "PAGESIZE")
+
+        log.debug("Resetting plugin to default.")
+        plugin = NagiosPlugin()
+        self.assertEqual(plugin.shortname, "TEST_PLUGIN_01")
+
+        log.debug("Creating plugin with a shortname 'SIZE' on init.")
+        plugin = NagiosPlugin(shortname = 'SIZE')
+        self.assertEqual(plugin.shortname, "SIZE")
+
+        log.debug("Creating plugin with a plugin name 'check_stuff'")
+        plugin = NagiosPlugin(plugin = 'check_stuff')
+        self.assertEqual(plugin.shortname, "STUFF")
+
+        log.debug("Creating plugin with a shortname 'SIZE' and a plugin " +
+                "name 'check_stuff' on init.")
+        plugin = NagiosPlugin(shortname = 'SIZE', plugin = 'check_stuff')
+        self.assertEqual(plugin.shortname, "SIZE")
+
+        log.debug("Setting thresholds to warn if < 10, critical if > 25.")
+        t = plugin.set_thresholds(warning = "10:25", critical = "~:25")
+        if not isinstance(t, NagiosThreshold):
+            self.fail("Not a NagiosThreshold object: %r" % (t))
+
+        log.debug("Adding performance data size ...")
+        plugin.add_perfdata(
+                label = "size", value = 1, uom = "kB", threshold = t
+        )
+        pdata = plugin.all_perfoutput()
+        log.debug("Got performance data: %r", pdata)
+        self.assertEqual(pdata, 'size=1kB;10:25;~:25')
+
+        log.debug("Adding performance data time ...")
+        plugin.add_perfdata(label = "time", value = 3.52, threshold = t)
+        pdata = plugin.all_perfoutput()
+        log.debug("Got performance data: %r", pdata)
+        self.assertEqual(pdata, 'size=1kB;10:25;~:25 time=3.52;10:25;~:25')
+
+    #--------------------------------------------------------------------------
+    def test_plugin_threshold_checks(self):
+
+        log.info("Testing plugin threshold checks ...")
+
+        plugin = NagiosPlugin(shortname = 'SIZE', plugin = 'check_stuff')
+        log.debug("Setting thresholds to warn if < 10, critical if > 25.")
+        t = plugin.set_thresholds(warning = "10:25", critical = "~:25")
+
+        expected_results = OrderedDict()
+        expected_results[-1] = nagios.state.warning
+        expected_results[1] = nagios.state.warning
+        expected_results[20] = nagios.state.ok
+        expected_results[25] = nagios.state.ok
+        expected_results[26] = nagios.state.critical
+        expected_results[30] = nagios.state.critical
+
+        for value in expected_results.keys():
+            ecpected_result = expected_results[value]
+            got_result = t.get_status(value)
+            log.debug("Checking value %d, expect result %d, got result %d.",
+                    value, ecpected_result, got_result)
+            self.assertEqual(got_result, ecpected_result)
 
 #==============================================================================
 
@@ -135,6 +144,10 @@ if __name__ == '__main__':
 
     suite.addTests(loader.loadTestsFromName(
             'test_plugin_01.TestNagiosPlugin.test_plugin_object'))
+    suite.addTests(loader.loadTestsFromName(
+            'test_plugin_01.TestNagiosPlugin.test_plugin_object_props'))
+    suite.addTests(loader.loadTestsFromName(
+            'test_plugin_01.TestNagiosPlugin.test_plugin_threshold_checks'))
 
     runner = unittest.TextTestRunner(verbosity = verbose)
 
