900ade3d9bd80af20472420dd1bad1dc4e417aca
Frank Brehm <frank.brehm@profitbricks.com>
2013-01-04 16:23:21 +0100

    Added test/test_plugin_03.py

---
+++ test/test_plugin_03.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@organization: Profitbricks GmbH
+@copyright: © 2010-2013 by Profitbricks GmbH
+@license: GPL3
+@summary: test script (and module) for unit tests on NagiosPlugin objects
+'''
+
+import unittest
+import os
+import sys
+import logging
+import re
+
+from collections import OrderedDict
+
+libdir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+sys.path.insert(0, libdir)
+
+import general
+from general import ColoredFormatter, pp, get_arg_verbose, init_root_logger
+from general import NeedConfig, NeedTmpConfig
+
+import nagios
+from nagios import FakeExitError
+
+from nagios.plugin import NagiosPluginError
+from nagios.plugin import NagiosPlugin
+
+from nagios.plugin.threshold import NagiosThreshold
+
+from nagios.plugin.config import NoConfigfileFound
+
+log = logging.getLogger(__name__)
+
+#==============================================================================
+class TestNagiosPlugin3(NeedConfig):
+
+    #--------------------------------------------------------------------------
+    def setUp(self):
+
+        super(TestNagiosPlugin3, self).setUp()
+
+        self.plugin_name = 'TEST_CHECK_MESSAGES'
+        self.plugin = NagiosPlugin(shortname = self.plugin_name)
+
+    #--------------------------------------------------------------------------
+    def test_plugin_object(self):
+
+        log.info("Testing NagiosPlugin object shortname %r.", self.plugin_name)
+        self.assertEqual(self.plugin.shortname, self.plugin_name)
+
+#==============================================================================
+
+if __name__ == '__main__':
+
+    verbose = get_arg_verbose()
+    init_root_logger(verbose)
+
+    log.info("Starting tests ...")
+
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    suite.addTests(loader.loadTestsFromName(
+            'test_plugin_03.TestNagiosPlugin3.test_plugin_object'))
+
+    runner = unittest.TextTestRunner(verbosity = verbose)
+
+    result = runner.run(suite)
+
+#==============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 nu
