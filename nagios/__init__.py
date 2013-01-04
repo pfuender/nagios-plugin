8f9f44c69ffb9baebf50ea8a3b78fd406e31f9d2
Frank Brehm <frank.brehm@profitbricks.com>
2013-01-04 12:07:38 +0100

    Version bump monor version

--- nagios/__init__.py
+++ nagios/__init__.py
@@ -10,7 +10,7 @@
 __author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
 __copyright__ = '© 2010-2013 by profitbricks.com'
 __contact__ = 'frank.brehm@profitbricks.com'
-__version__ = '0.2.7-2'
+__version__ = '0.3.0'
 __license__ = 'GPL3'
 
 #==============================================================================
