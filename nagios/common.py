6771369a8430666e97228534791e406d7890e143
Frank Brehm <frank.brehm@profitbricks.com>
2013-01-07 15:13:25 +0100

    Added function caller_search_path() to nagios/common.py

--- nagios/common.py
+++ nagios/common.py
@@ -31,15 +31,72 @@ log = logging.getLogger(__name__)
 
 # Currently the only function
 def pp(value):
-    '''
+    """
     Returns a pretty print string of the given value.
 
     @return: pretty print string
     @rtype: str
-    '''
+    """
 
     pretty_printer = pprint.PrettyPrinter(indent=4)
     return pretty_printer.pformat(value)
+
+#==============================================================================
+def caller_search_path(prepend = None, append = None):
+    """
+    Builds a search path for executables from environment $PATH
+    including some standard paths.
+
+    @param prepend: a list of search paths prepending to the result list
+    @type prepend: list of str
+    @param append: a list of search paths appending to the result list
+    @type append: list of str
+
+    @return: list of existing search paths
+    @rtype: list
+
+    """
+
+    path_list = []
+    search_path = os.environ['PATH']
+    if not search_path:
+        search_path = os.defpath
+
+    search_path_list = []
+
+    if prepend:
+        for d in prepend:
+            search_path_list.append(d)
+
+    for d in search_path.split(os.pathsep):
+        search_path_list.append(d)
+
+    default_path = [
+        '/bin',
+        '/usr/bin',
+        '/usr/local/bin',
+        '/sbin',
+        '/usr/sbin',
+        '/usr/local/sbin',
+    ]
+
+    for d in default_path:
+        search_path_list.append(d)
+
+    if append:
+        for d in append:
+            search_path_list.append(d)
+
+    for d in search_path_list:
+        if not os.path.exists(d):
+            continue
+        if not os.path.isdir(d):
+            continue
+        d_abs = os.path.realpath(d)
+        if not d_abs in path_list:
+            path_list.append(d_abs)
+
+    return path_list
 
 #==============================================================================
 
