2fdf017123b71a9d47efba3bb6da3a0320325ee4
Frank Brehm <frank.brehm@profitbricks.com>
2013-01-07 15:35:14 +0100

    Started with ExtNagiosPlugin in nagios/plugins/__init__.py

--- nagios/plugins/__init__.py
+++ nagios/plugins/__init__.py
@@ -24,6 +24,8 @@ from nagios.common import pp
 from nagios.plugin import NagiosPluginError
 from nagios.plugin import NagiosPlugin
 
+from nagios.plugin.argparser import lgpl3_licence_text, default_timeout
+
 #---------------------------------------------
 # Some module variables
 
@@ -37,7 +39,109 @@ class ExtNagiosPluginError(NagiosPluginError):
 
     pass
 
+#==============================================================================
+class ExtNagiosPlugin(NagiosPlugin):
+    """
+    An extended Nagios plugin class.
 
+    """
+
+    #--------------------------------------------------------------------------
+    def __init__(self, usage = None, shortname = None,
+            version = nagios.__version__, url = None, blurb = None,
+            licence = lgpl3_licence_text, extra = None, plugin = None,
+            timeout = default_timeout, verbose = 0):
+        """
+        Constructor of the ExtNagiosPlugin class.
+
+        @param usage: Short usage message used with --usage/-? and with missing
+                      required arguments, and included in the longer --help
+                      output. Can include %(prog)s placeholder which will be
+                      replaced with the plugin name, e.g.:
+
+                          usage = 'Usage: %(prog)s -H <hostname> -p <ports> [-v]'
+        @type usage: str
+        @param shortname: the shortname of the plugin
+        @type shortname: str
+        @param version: Plugin version number, included in the --version/-V
+                        output, and in the longer --help output. e.g.::
+
+                            $ ./check_tcp_range --version
+                            check_tcp_range 0.2 [http://www.openfusion.com.au/labs/nagios/]
+        @type version: str
+        @param url: URL for info about this plugin, included in the
+                    --version/-V output, and in the longer --help output.
+                    Maybe omitted.
+        @type url: str or None
+        @param blurb: Short plugin description, included in the longer
+                      --help output. Maybe omitted.
+        @type blurb: str or None
+        @param licence: License text, included in the longer --help output. By
+                        default, this is set to the standard nagios plugins
+                        LGPLv3 licence text.
+        @type licence: str or None
+        @param extra: Extra text to be appended at the end of the longer --help
+                      output, maybe omitted.
+        @type extra: str or None
+        @param plugin: Plugin name. This defaults to the basename of your plugin.
+        @type plugin: str or None
+        @param timeout: Timeout period in seconds, overriding the standard
+                        timeout default (15 seconds).
+        @type timeout: int
+        @param verbose: verbosity level inside the module
+        @type verbose: int
+
+        """
+
+        self._verbose = 0
+        """
+        @ivar: The verbosity level inside the module.
+        @type: int
+        """
+        if value:
+            self.value = value
+
+        super(ExtNagiosPlugin, self).__init__(
+                usage = usage,
+                shortname = shortname,
+                version = version,
+                url = url,
+                blurb = blurb,
+                licence = licence,
+                extra = extra,
+                plugin = plugin,
+                timeout = timeout
+        )
+
+    #------------------------------------------------------------
+    @property
+    def verbose(self):
+        """The verbosity level inside the module."""
+        return self._verbose
+
+    @verbose.setter
+    def verbose(self, value):
+        val = int(value)
+        if val < 0:
+            log.warn("A negative verbose level (%r)is not supported.", value)
+        else:
+            self._verbose = val
+
+    #--------------------------------------------------------------------------
+    def as_dict(self):
+        """
+        Typecasting into a dictionary.
+
+        @return: structure as dict
+        @rtype:  dict
+
+        """
+
+        d = super(ExtNagiosPlugin, self).as_dict()
+
+        d['verbose'] = self.verbose
+
+        return d
 
 #==============================================================================
 
