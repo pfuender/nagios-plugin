2cad0dd9a80d96f414bcd2080eb165437ba51eff
Frank Brehm <frank.brehm@profitbricks.com>
2013-10-25 08:55:59 +0000

    Started with nagios/plugins/check_softwareraid.py

---
+++ nagios/plugins/check_softwareraid.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: © 2010 - 2013 by Frank Brehm, Berlin
+@summary: Module for CheckSoftwareRaidPlugin class
+"""
+
+# Standard modules
+import os
+import sys
+import logging
+import textwrap
+import pwd
+import re
+import locale
+import stat
+
+from numbers import Number
+
+# Third party modules
+
+# Own modules
+
+import nagios
+from nagios import BaseNagiosError
+
+from nagios.common import pp, caller_search_path
+
+from nagios.plugin import NagiosPluginError
+from nagios.plugin import NPReadTimeoutError
+
+from nagios.plugin.range import NagiosRange
+
+from nagios.plugin.threshold import NagiosThreshold
+
+from nagios.plugin.argparser import default_timeout
+
+from nagios.plugins import ExtNagiosPluginError
+from nagios.plugins import ExecutionTimeoutError
+from nagios.plugins import CommandNotFoundError
+from nagios.plugins import ExtNagiosPlugin
+
+#---------------------------------------------
+# Some module variables
+
+__version__ = '0.1.0'
+
+log = logging.getLogger(__name__)
+
+#==============================================================================
+class CheckSoftwareRaidPlugin(ExtNagiosPlugin):
+    """
+    A special NagiosPlugin class for checking the state of one or all  Linux
+    software RAID devices (MD devices).
+    """
+
+    #--------------------------------------------------------------------------
+    def __init__(self):
+        """
+        Constructor of the CheckSoftwareRaidPlugin class.
+        """
+
+        usage = """\
+        %(prog)s [-v] [<MD device>]
+        """
+        usage = textwrap.dedent(usage).strip()
+        usage += '\n       %(prog)s --usage'
+        usage += '\n       %(prog)s --help'
+
+        blurb = "Copyright (c) 2013 Frank Brehm, Berlin.\n\n"
+        blurb += "Checks the state of one or all  Linux software RAID devices."
+
+        super(CheckSoftwareRaidPlugin, self).__init__(
+                usage = usage, blurb = blurb,
+        )
+
+
+#==============================================================================
+
+if __name__ == "__main__":
+
+    pass
+
+#==============================================================================
+
+# vim: filetype=python ts=4 expandtab shiftwidth=4 softtabstop=4
