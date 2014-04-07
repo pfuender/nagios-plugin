db87e316ff80dead064894709a9e154360840486
Thomas Moericke <thomas.moericke@profitbricks.com>
2014-04-07 11:22:05 +0200

    version bump

--- nagios/__init__.py
+++ nagios/__init__.py
@@ -10,7 +10,7 @@
 __author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
 __copyright__ = '© 2010 - 2014 by profitbricks.com'
 __contact__ = 'frank.brehm@profitbricks.com'
-__version__ = '1.5.25'
+__version__ = '1.5.26'
 __license__ = 'GPL3'
 
 #==============================================================================
