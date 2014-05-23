4901b07571e0b04c7473a47778dce42b82d219d2
Frank Brehm <frank.brehm@profitbricks.com>
2014-05-23 13:43:25 +0000

    Included retreiving of storage maps from API in nagios/plugins/check_pb_storage_exports.py

--- nagios/plugins/check_pb_storage_exports.py
+++ nagios/plugins/check_pb_storage_exports.py
@@ -64,7 +64,7 @@ from dcmanagerclient.client import RestApiError
 #---------------------------------------------
 # Some module variables
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __copyright__ = 'Copyright (c) 2014 Frank Brehm, Berlin.'
 
 DEFAULT_WARN_ERRORS = 0
@@ -126,6 +126,8 @@ class CheckPbStorageExportsPlugin(BaseDcmClientPlugin):
         """
 
         self.all_api_exports = {}
+        self.storage_exports = []
+        self.image_exports = []
         self.count = {}
 
         # Some commands are missing
@@ -289,7 +291,181 @@ class CheckPbStorageExportsPlugin(BaseDcmClientPlugin):
                 'error': 0,
         }
 
+        self.get_api_storage_exports()
+
         self.exit(state, out)
+
+    #--------------------------------------------------------------------------
+    def get_api_storage_exports(self):
+
+        self.storage_exports = []
+        api_volumes = {}
+
+        key_replicated = 'replicated'
+        key_replicas = 'replicas'
+        key_storage_server = 'storage_server'
+        key_guid = 'guid'
+        key_uuid = 'uuid'
+        key_pstorage_name = 'pstorage_name'
+        key_vstorage_uuid = 'vstorage_uuid'
+        key_pserver_name = 'pserver_name'
+        if sys.version_info[0] <= 2:
+            key_replicated = key_replicated.decode('utf-8')
+            key_replicas = key_replicas.decode('utf-8')
+            key_storage_server = key_storage_server.decode('utf-8')
+            key_guid = key_guid.decode('utf-8')
+            key_uuid = key_uuid.decode('utf-8')
+            key_pstorage_name = key_pstorage_name.decode('utf-8')
+            key_vstorage_uuid = key_vstorage_uuid.decode('utf-8')
+            key_pserver_name = key_pserver_name.decode('utf-8')
+
+        log.debug("Retrieving storage volumes from API ...")
+        storages = None
+        try:
+            storages = self.api.vstorages(pstorage = self.hostname)
+        except RestApiError as e:
+            self.die(str(e))
+        except Exception as e:
+            self.die("%s: %s" % (e.__class__.__name__, e))
+
+        first = True
+        for stor in storages:
+
+            """
+            {   'cloned_from': None,
+                'cluster': 'de-ka-cluster-01',
+                'contract': 31721232,
+                'creation_date': '2014-05-21T04:10:38.399',
+                'modification_date': '2014-05-21T04:15:35.620',
+                'name': 'DWDCStorage_EMEA_D_Mirror',
+                'os_type': None,
+                'physical_server': 'pserver117',
+                'region': 'europe',
+                'replicas': [   {   'guid': '600144f0-0001-dc9b-0121-e09d11e3920c',
+                                    'storage_server': 'storage201',
+                                    'virtual_state': 'AVAILABLE'},
+                                {   'guid': '600144f0-0001-dc98-6910-e09d11e3920c',
+                                    'storage_server': 'storage103',
+                                    'virtual_state': 'AVAILABLE'}],
+                'replicated': True,
+                'size': 716800,
+                'uuid': 'e7abbe07-3d3e-4468-9af4-1bfe8af418dc',
+                'virtual_network': '61ba3819-d719-4986-b59d-c6178a32aabe'}
+            """
+
+            vl = 4
+            if first:
+                vl = 2
+
+            if self.verbose > vl:
+                log.debug("Got Storage volume from API:\n%s", pp(stor))
+
+            replicated = stor[key_replicated]
+            vol_uuid = uuid.UUID(stor[key_uuid])
+
+            guid = None
+            for replica in  stor[key_replicas]:
+                hn = replica[key_storage_server]
+                if sys.version_info[0] <= 2:
+                    hn = hn.encode('utf-8')
+                if hn == self.hostname:
+                    guid = uuid.UUID(replica[key_guid])
+                    break
+
+            if not guid:
+                log.debug("No valid GUID found for storage volume:\n%s", pp(stor))
+                continue
+
+            vol = {
+                'guid': guid,
+                'replicated': replicated,
+            }
+            api_volumes[vol_uuid] = vol
+
+            if self.verbose > vl:
+                log.debug("Transformed Storage volume %r:\n%s", vol_uuid, pp(vol))
+
+            if first:
+                first = False
+
+
+        log.debug("Retrieving storage mappings from API ...")
+        maps = None
+        try:
+            maps = self.api.vstorage_maps(pstorage = self.hostname)
+        except RestApiError as e:
+            self.die(str(e))
+        except Exception as e:
+            self.die("%s: %s" % (e.__class__.__name__, e))
+
+        first = True
+
+        for mapping in maps:
+
+            """
+            {   'boot_order': 1,
+                'creation_date': '2014-05-23T08:42:32.770',
+                'dc_name': 'BKK Pfalz',
+                'modification_date': '2014-05-23T08:42:32.770',
+                'mount_state': 'AVAILABLE',
+                'mount_type': 'VIRTIO',
+                'mount_uuid': '8b30c7e8-f9fb-41b3-8b1d-723e1c41ee99',
+                'network_uuid': '485abc1d-ec56-81d0-24cc-621cde8f34dc',
+                'order_nr': 1,
+                'pserver_name': 'pserver220',
+                'pserver_uuid': '48385147-3600-0030-48FF-003048FF26B2',
+                'pstorage_name': 'storage201',
+                'pstorage_uuid': '49434D53-0200-9071-2500-71902500F26D',
+                'replicated': True,
+                'size_mb': 102400,
+                'vm_name': 'replacement',
+                'vm_uuid': '001f75f0-fa36-40cc-a628-060d2ecdccc1',
+                'vstorage_guid': None,
+                'vstorage_name': 'BKK Storage 2',
+                'vstorage_uuid': '64f9dd3a-6db9-4405-a023-a0d32203c2aa'}
+            """
+
+            if mapping[key_pstorage_name] != self.hostname:
+                continue
+            if mapping[key_pserver_name] is None:
+                continue
+
+            vl = 4
+            if first:
+                vl = 2
+
+            if self.verbose > vl:
+                log.debug("Got Storage mapping from API:\n%s", pp(mapping))
+
+            vol_uuid = uuid.UUID(mapping[key_vstorage_uuid])
+
+            if not vol_uuid in api_volumes:
+                log.error("No volume for mapping of %r found.", vol_uuid)
+                continue
+
+            guid = api_volumes[vol_uuid]['guid']
+            scst_devname =  crc64_digest(str(guid))
+            pserver = mapping[key_pserver_name]
+            if sys.version_info[0] <= 2:
+                pserver = pserver.decode('utf-8')
+
+            m = {
+                'uuid': vol_uuid,
+                'guid': guid,
+                'scst_devname': scst_devname,
+                'replicated': api_volumes[vol_uuid]['replicated'],
+                'pserver': pserver,
+            }
+            self.storage_exports.append(m)
+
+            if self.verbose > vl:
+                log.debug("Transformed storage mapping:\n%s",  pp(m))
+
+            if first:
+                first = False
+
+        log.debug("Finished retrieving storage mappings from API, found %d mappings.",
+                len(self.storage_exports))
 
 #==============================================================================
 
