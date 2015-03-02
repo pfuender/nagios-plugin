01beaf34ca1bb31d68cc082164b15538b4f2deaf
Frank Brehm <frank.brehm@profitbricks.com>
2015-03-02 17:33:46 +0100

    Flake8 for nagios/plugins/check_megaraid_hs.py

--- nagios/plugins/check_megaraid_hs.py
+++ nagios/plugins/check_megaraid_hs.py
@@ -9,54 +9,37 @@
 """
 
 # Standard modules
-import os
-import sys
 import re
 import logging
 import textwrap
-
-from numbers import Number
 
 # Third party modules
 
 # Own modules
 
 import nagios
-from nagios import BaseNagiosError
-
-from nagios.common import pp, caller_search_path
-
-from nagios.plugin import NagiosPluginError
-
-from nagios.plugin.functions import max_state
 
 from nagios.plugin.range import NagiosRange
-
-from nagios.plugin.threshold import NagiosThreshold
-
-from nagios.plugin.extended import ExtNagiosPluginError
-from nagios.plugin.extended import ExecutionTimeoutError
-from nagios.plugin.extended import CommandNotFoundError
-from nagios.plugin.extended import ExtNagiosPlugin
 
 import nagios.plugins.check_megaraid
 from nagios.plugins.check_megaraid import CheckMegaRaidPlugin
 
-#---------------------------------------------
+# --------------------------------------------
 # Some module variables
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 log = logging.getLogger(__name__)
 
-#==============================================================================
+
+# =============================================================================
 class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
     """
     A special NagiosPlugin class for checking the number of hotspare drives on
     a LSI MegaRaid adapter.
     """
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def __init__(self):
         """
         Constructor of the CheckMegaRaidHotsparePlugin class.
@@ -73,9 +56,9 @@ class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
         blurb += "Checks the number of hotspare drives on a LSI MegaRaid adapter."
 
         super(CheckMegaRaidHotsparePlugin, self).__init__(
-                shortname = 'MEGARAID_HOTSPARE',
-                usage = usage, blurb = blurb,
-                version = __version__,
+            shortname='MEGARAID_HOTSPARE',
+            usage=usage, blurb=blurb,
+            version=__version__,
         )
 
         self._critical_number = NagiosRange('1:')
@@ -84,21 +67,21 @@ class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
 
         self._add_args()
 
-    #------------------------------------------------------------
+    # -----------------------------------------------------------
     @property
     def critical_number(self):
         """The number of hotspare drives, where it becomes critical, if
         the number of existing hotspares is below."""
         return self._critical_number
 
-    #------------------------------------------------------------
+    # -----------------------------------------------------------
     @property
     def warning_number(self):
         """The number of hotspare drives, where it becomes a warning, if
         the number of existing hotspares is below."""
         return self._warning_number
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def as_dict(self):
         """
         Typecasting into a dictionary.
@@ -115,48 +98,48 @@ class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
 
         return d
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def _add_args(self):
         """
         Adding all necessary arguments to the commandline argument parser.
         """
 
         help_c = """\
-                The number of hotspare drives, where it becomes critical,
-                if the number of existing hotspares is below (mandantory,
-                default: '%(default)s').
-                """
+            The number of hotspare drives, where it becomes critical,
+            if the number of existing hotspares is below (mandantory,
+            default: '%(default)s').
+            """
         help_c = textwrap.dedent(help_c).replace('\n', ' ').strip()
         self.add_arg(
-                '-c', '--critical',
-                metavar = 'DRIVES:',
-                dest = 'critical',
-                required = True,
-                type = NagiosRange,
-                default = self.critical_number,
-                help = help_c,
+            '-c', '--critical',
+            metavar='DRIVES:',
+            dest='critical',
+            required=True,
+            type=NagiosRange,
+            default=self.critical_number,
+            help=help_c,
         )
 
         help_w = """\
-                The number of hotspare drives, where it becomes a warning,
-                if the number of existing hotspares is below (mandantory,
-                default: '%(default)s').
-                """
+            The number of hotspare drives, where it becomes a warning,
+            if the number of existing hotspares is below (mandantory,
+            default: '%(default)s').
+            """
         help_w = textwrap.dedent(help_w).replace('\n', ' ').strip()
         self.add_arg(
-                '-w', '--warning',
-                metavar = 'DRIVES:',
-                dest = 'warning',
-                required = True,
-                type = NagiosRange,
-                default = self.warning_number,
-                help = help_w,
+            '-w', '--warning',
+            metavar='DRIVES:',
+            dest='warning',
+            required=True,
+            type=NagiosRange,
+            default=self.warning_number,
+            help=help_w,
         )
 
         super(CheckMegaRaidHotsparePlugin, self)._add_args()
 
-    #--------------------------------------------------------------------------
-    def parse_args(self, args = None):
+    # -------------------------------------------------------------------------
+    def parse_args(self, args=None):
         """
         Executes self.argparser.parse_args().
 
@@ -171,31 +154,33 @@ class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
         super(CheckMegaRaidHotsparePlugin, self).parse_args(args)
 
         crit = self.argparser.args.critical
-        if not crit.end is None:
-            msg = ("The critical hot spare number must be given with an " +
-                    "ending colon, e.g. '1:' (given %s).") % (crit)
+        if crit.end is not None:
+            msg = (
+                "The critical hot spare number must be given with an "
+                "ending colon, e.g. '1:' (given %s).") % (crit)
             self.die(msg)
         self._critical_number = crit
 
         warn = self.argparser.args.warning
-        if not warn.end is None:
-            msg = ("The warning hot spare number must be given with an " +
-                    "ending colon, e.g. '2:' (given %s).") % (warn)
+        if warn.end is not None:
+            msg = (
+                "The warning hot spare number must be given with an "
+                "ending colon, e.g. '2:' (given %s).") % (warn)
             self.die(msg)
         self._warning_number = warn
 
         if crit.start > warn.start:
-            msg = ("The warning number must be greater than or equal to " +
-                    "the critical number (given warning: '%s', critical " +
-                    "'%s').") % (warn, crit)
+            msg = (
+                "The warning number must be greater than or equal to the critical number "
+                "(given warning: '%s', critical '%s').") % (warn, crit)
             self.die(msg)
 
         self.set_thresholds(
-                warning = warn,
-                critical = crit,
+            warning=warn,
+            critical=crit,
         )
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def call(self):
         """
         Method to call the plugin directly.
@@ -203,7 +188,7 @@ class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
 
         state = nagios.state.ok
         out = "Number of existing hotspares of MegaRaid adapter %d seems to be okay." % (
-                self.adapter_nr)
+            self.adapter_nr)
 
         # Slot Number: 23
         re_slot = re.compile(r'^\s*Slot\s+Number\s*:\s*\d+', re.IGNORECASE)
@@ -234,30 +219,30 @@ class CheckMegaRaidHotsparePlugin(CheckMegaRaidPlugin):
 
         state = self.threshold.get_status(found_hotspares)
         out = "found %d hotspare(s) " % (found_hotspares)
-        out += "(warning: <%d, critical: <%d)." %  (self.threshold.warning.start,
-                self.threshold.critical.start)
+        out += "(warning: <%d, critical: <%d)." % (
+            self.threshold.warning.start, self.threshold.critical.start)
 
         self.add_perfdata(
-                label = 'hotspares',
-                value = found_hotspares,
-                uom = '',
-                threshold = self.threshold,
+            label='hotspares',
+            value=found_hotspares,
+            uom='',
+            threshold=self.threshold,
         )
 
         self.add_perfdata(
-                label = 'drives_total',
-                value = drives_total,
-                uom = '',
+            label='drives_total',
+            value=drives_total,
+            uom='',
         )
 
         self.exit(state, out)
 
-#==============================================================================
+# =============================================================================
 
 if __name__ == "__main__":
 
     pass
 
-#==============================================================================
+# =============================================================================
 
 # vim: fileencoding=utf-8 filetype=python ts=4 et
