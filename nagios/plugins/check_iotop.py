22437fc7054aa9fa4aee5451cbb7910778416485
Frank Brehm <frank.brehm@profitbricks.com>
2015-03-02 16:35:55 +0100

    Flake8 for nagios/plugins/check_iotop.py

--- nagios/plugins/check_iotop.py
+++ nagios/plugins/check_iotop.py
@@ -10,65 +10,42 @@
 
 # Standard modules
 import os
-import sys
 import re
 import logging
 import textwrap
 import time
 
-from numbers import Number
-
 # Third party modules
-from iotop.data import find_uids, TaskStatsNetlink, ProcessList, Stats
-from iotop.data import ThreadInfo
+from iotop.data import TaskStatsNetlink, ProcessList
 
 # Own modules
 
 import nagios
-from nagios import BaseNagiosError
 
-from nagios.common import pp, caller_search_path
-
-from nagios.plugin import NagiosPluginError
-
-from nagios.plugin.range import NagiosRange
+from nagios.common import pp
 
 from nagios.plugin.threshold import NagiosThreshold
 
-from nagios.plugin.extended import ExtNagiosPluginError
-from nagios.plugin.extended import ExecutionTimeoutError
-from nagios.plugin.extended import CommandNotFoundError
 from nagios.plugin.extended import ExtNagiosPlugin
 
-from nagios.plugin.config import NoConfigfileFound
-from nagios.plugin.config import NagiosPluginConfig
-
-#---------------------------------------------
+# --------------------------------------------
 # Some module variables
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __copyright__ = 'Copyright (c) 2015 Frank Brehm, Berlin.'
 
 DEFAULT_TIMEOUT = 60
 
 log = logging.getLogger(__name__)
 
-###############################################################################
+
+# #############################################################################
 class IotopOptions(object):
 
-    def __init__(self,
-            only = False,
-            batch = None,
-            iterations = None,
-            delay_seconds = 1,
-            pids = None,
-            users = None,
-            processes = False,
-            accumulated = False,
-            kilobytes = False,
-            time = None,
-            quiet = 0,
-            profile = False):
+    def __init__(
+        self, only=False, batch=None, iterations=None, delay_seconds=1, pids=None,
+            users=None, processes=False, accumulated=False, kilobytes=False,
+            time=None, quiet=0, profile=False):
 
         self.only = bool(only)
 
@@ -110,14 +87,15 @@ class IotopOptions(object):
 
         self.profile = bool(profile)
 
-#==============================================================================
+
+# =============================================================================
 class CheckIotopPlugin(ExtNagiosPlugin):
     """
     A special NagiosPlugin class for checking I/O utilization of processes
     with iotop
     """
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def __init__(self):
         """
         Constructor of the CheckIotopPlugin class.
@@ -142,9 +120,7 @@ class CheckIotopPlugin(ExtNagiosPlugin):
         """
         blurb = textwrap.dedent(blurb).strip()
 
-        super(CheckIotopPlugin, self).__init__(
-                usage = usage, blurb = blurb,
-        )
+        super(CheckIotopPlugin, self).__init__(usage=usage, blurb=blurb)
 
         self.warning = (1, 3, 5)
         self.critical = (1, 5, 10)
@@ -156,66 +132,69 @@ class CheckIotopPlugin(ExtNagiosPlugin):
         self.process_list = None
         self.processes = []
         self.process_count = {
-                '90': 0,
-                '50': 0,
-                '10': 0,
-                '0': 0,
-                'total': 0,
+            '90': 0,
+            '50': 0,
+            '10': 0,
+            '0': 0,
+            'total': 0,
         }
 
         self._add_args()
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def _add_args(self):
         """
         Adding all necessary arguments to the commandline argument parser.
         """
 
-        msg_tpl = "Generate a %s state if the number of processes with an "
-        msg_tpl += "I/O-delay of 90/50/10 %%%% is higher as the appropriate "
-        msg_tpl += "threshold (default: %%(default)s)."
+        msg_tpl = (
+            "Generate a %s state if the number of processes with an "
+            "I/O-delay of 90/50/10 %%%% is higher as the appropriate "
+            "threshold (default: %%(default)s).")
 
         self.add_arg(
-                '-w', '--warning',
-                metavar = 'IO_DELAY_90,IO_DELAY_50,IO_DELAY_10',
-                dest = 'warning',
-                required = True,
-                default = ','.join(map(lambda x: ("%d" % (x)), self.warning)),
-                help = msg_tpl % ('warning'),
+            '-w', '--warning',
+            metavar='IO_DELAY_90,IO_DELAY_50,IO_DELAY_10',
+            dest='warning',
+            required=True,
+            default=','.join(map(lambda x: ("%d" % (x)), self.warning)),
+            help=msg_tpl % ('warning'),
         )
 
         self.add_arg(
-                '-c', '--critical',
-                metavar = 'IO_DELAY_90,IO_DELAY_50,IO_DELAY_10',
-                dest = 'critical',
-                required = True,
-                default = ','.join(map(lambda x: ("%d" % (x)), self.critical)),
-                help = msg_tpl % ('critical'),
+            '-c', '--critical',
+            metavar='IO_DELAY_90,IO_DELAY_50,IO_DELAY_10',
+            dest='critical',
+            required=True,
+            default=','.join(map(lambda x: ("%d" % (x)), self.critical)),
+            help=msg_tpl % ('critical'),
         )
 
         self.add_arg(
-                '-d', '--delay',
-                metavar = 'SEC',
-                type = float,
-                dest = 'delay',
-                default = self.delay,
-                required = True,
-                help = ("Delay between iterations in seconds of refreshing " +
-                        "the process list (default: %(default).1f sec)."),
+            '-d', '--delay',
+            metavar='SEC',
+            type=float,
+            dest='delay',
+            default=self.delay,
+            required=True,
+            help=(
+                "Delay between iterations in seconds of refreshing "
+                "the process list (default: %(default).1f sec)."),
         )
 
         self.add_arg(
-                '-i', '--iterations',
-                metavar = 'NUM',
-                type = int,
-                dest = 'iterations',
-                default = self.iterations,
-                required = True,
-                help = ("Set the number of iterations before calculating "
-                        "the reult of this check (default: %(default)s)."),
+            '-i', '--iterations',
+            metavar='NUM',
+            type=int,
+            dest='iterations',
+            default=self.iterations,
+            required=True,
+            help=(
+                "Set the number of iterations before calculating "
+                "the reult of this check (default: %(default)s)."),
         )
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def __call__(self):
         """
         Method to call the plugin directly.
@@ -234,28 +213,30 @@ class CheckIotopPlugin(ExtNagiosPlugin):
             self.die(msg)
         self.iterations = self.argparser.args.iterations
 
-        msg_tpl = ("The %s thresholds must be given in the form " +
-                "'IO_DELAY_90,IO_DELAY_50,IO_DELAY_10', where the " +
-                "values are integer values.")
+        msg_tpl = (
+            "The %s thresholds must be given in the form "
+            "'IO_DELAY_90,IO_DELAY_50,IO_DELAY_10', where the "
+            "values are integer values.")
         re_limits = re.compile(r'^\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*$')
 
         match = re_limits.search(self.argparser.args.warning)
         if not match:
             self.die(msg_tpl % ('warning'))
-        self.warning = (int(match.group(1)), int(match.group(2)),
-                int(match.group(3)))
+        self.warning = (
+            int(match.group(1)), int(match.group(2)), int(match.group(3)))
 
         match = re_limits.search(self.argparser.args.critical)
         if not match:
             self.die(msg_tpl % ('critical'))
-        self.critical = (int(match.group(1)), int(match.group(2)),
-                int(match.group(3)))
+        self.critical = (
+            int(match.group(1)), int(match.group(2)), int(match.group(3)))
 
         if self.verbose > 1:
-            log.debug("Got thresholds: warning: %r, critical: %r.",
-                    self.warning, self.critical)
-            log.debug("Number of iterations: %d, delay between: %.1f seconds.",
-                    self.iterations, self.delay)
+            log.debug(
+                "Got thresholds: warning: %r, critical: %r.", self.warning, self.critical)
+            log.debug(
+                "Number of iterations: %d, delay between: %.1f seconds.",
+                self.iterations, self.delay)
             log.debug("Initialisation complete.")
 
         if os.geteuid():
@@ -276,58 +257,60 @@ class CheckIotopPlugin(ExtNagiosPlugin):
             state = nagios.state.warning
 
         t_90 = NagiosThreshold(
-                warning = ":%d" % (self.warning[0]),
-                critical = ":%d" % (self.critical[0]),
+            warning=":%d" % (self.warning[0]),
+            critical=":%d" % (self.critical[0]),
         )
 
         t_50 = NagiosThreshold(
-                warning = ":%d" % (self.warning[1]),
-                critical = ":%d" % (self.critical[1]),
+            warning=":%d" % (self.warning[1]),
+            critical=":%d" % (self.critical[1]),
         )
 
         t_10 = NagiosThreshold(
-                warning = ":%d" % (self.warning[2]),
-                critical = ":%d" % (self.critical[2]),
+            warning=":%d" % (self.warning[2]),
+            critical=":%d" % (self.critical[2]),
         )
 
         self.add_perfdata(
-                label = 'total_procs',
-                value = self.process_count['total'],
+            label='total_procs',
+            value=self.process_count['total'],
         )
 
         self.add_perfdata(
-                label = 'iodelay_90',
-                value = self.process_count['90'],
-                threshold = t_90
+            label='iodelay_90',
+            value=self.process_count['90'],
+            threshold=t_90
         )
 
         self.add_perfdata(
-                label = 'iodelay_50',
-                value = self.process_count['50'],
-                threshold = t_50
+            label='iodelay_50',
+            value=self.process_count['50'],
+            threshold=t_50
         )
 
         self.add_perfdata(
-                label = 'iodelay_10',
-                value = self.process_count['10'],
-                threshold = t_10
+            label='iodelay_10',
+            value=self.process_count['10'],
+            threshold=t_10
         )
 
-        msg = "Total %(total)d procs, %(90)d procs with i/o delay >= 90%%, "
-        msg += "%(50)d procs with i/o delay >= 50%% and %(10)d procs with "
-        msg += "i/o delay >= 10%%."
+        msg = (
+            "Total %(total)d procs, %(90)d procs with i/o delay >= 90%%, "
+            "%(50)d procs with i/o delay >= 50%% and %(10)d procs with "
+            "i/o delay >= 10%%.")
         out = msg % self.process_count
 
         self.exit(state, out)
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def get_proc_stats(self):
 
         if self.verbose > 2:
             log.debug("Init of iotop objects  ...")
 
-        self.iotop_opts = IotopOptions(only = True, batch = True,
-                processes = True, iterations = self.iterations, accumulated = True)
+        self.iotop_opts = IotopOptions(
+            only=True, batch=True, processes=True,
+            iterations=self.iterations, accumulated=True)
 
         self.connection = TaskStatsNetlink(self.iotop_opts)
         self.process_list = ProcessList(self.connection, self.iotop_opts)
@@ -338,7 +321,7 @@ class CheckIotopPlugin(ExtNagiosPlugin):
                 log.debug("Refreshing processlist %d ...", j)
             total, actual = self.process_list.refresh_processes()
 
-    #--------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def evaluate_proc_stats(self):
 
         for proc in self.process_list.processes.values():
@@ -362,19 +345,19 @@ class CheckIotopPlugin(ExtNagiosPlugin):
             elif blkio_delay_percent >= 10:
                 self.process_count['10'] += 1
 
-        self.process_count['0'] = (self.process_count['total'] -
-                self.process_count['90'] - self.process_count['50'] -
-                self.process_count['10'])
+        self.process_count['0'] = (
+            self.process_count['total'] - self.process_count['90'] -
+            self.process_count['50'] - self.process_count['10'])
 
         if self.verbose > 1:
             log.debug("Got the following results:\n%s", pp(self.process_count))
 
-#==============================================================================
+# =============================================================================
 
 if __name__ == "__main__":
 
     pass
 
-#==============================================================================
+# =============================================================================
 
 # vim: fileencoding=utf-8 filetype=python ts=4 et sw=4
